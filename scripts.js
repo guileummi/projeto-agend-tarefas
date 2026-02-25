7fc25d34812ae453df548fa38b460d4e318b6df9
GUILHERME <111024179+guileummi@users.noreply.github.com>
2026-02-25 01:14:28 -0300

    criar função criarItem

--- scripts.js
+++ scripts.js
@@ -1,27 +1,50 @@
 function adicionar(){
+    /* função adicionar 
+         - pega o valor da caixa
+         - validar
+         - manda criar item
+         - manda salvar
+    */    
     var caixa = window.document.getElementById('itaf')
-    var lista = window.document.getElementById('lista')
     var valorCaixa = caixa.value
 
     if(valorCaixa === ''){
         window.alert('[ERROR] Valor vazio')
         return
     }
-
-    var item = document.createElement('li')
-    item.innerHTML = valorCaixa +
-    '<span onclick="concluir(this)">✅</span>' +
-    '<span onclick="deletar(this)">❌</span>'
-
-    lista.appendChild(item)
+    
+    criarItem(valorCaixa, false)
+    salvaLocalStorage()
 
     window.document.getElementById('itaf').value = ''
 }
 
-function concluir(item){
-    item.parentElement.classList.toggle('concluida')
+function criarItem(texto, concluida){
+    /* função criarItem
+        - criar o item (li)
+        - add texto
+        - add botoes
+        -aplicar classe (opcional)
+    */
+    var lista = window.document.getElementById('lista')
+    var item = document.createElement('li')
+    item.innerHTML = valorCaixa +
+    '<span onclick="concluir(this)">✅</span>' +
+    '<span onclick="deletar(this)">❌</span>'
+    
+    if(concluida === 'true'){
+        item.classList.add('concluida')
+    }
+
+    lista.appendChild(item)
 }
 
-function deletar(item){
-    item.parentElement.remove()
+function concluir(botao){
+    botao.parentElement.classList.toggle('concluida')
+    salvaLocalStorage()
+}
+
+function deletar(botao){
+    botao.parentElement.remove()
+    salvaLocalStorage()
 }
